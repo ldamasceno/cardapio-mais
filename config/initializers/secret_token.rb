1ea7869b25563b8d66e1f4b235b6c19ca6b4a8a0
ldamasceno <lui.damasceno@gmail.com>
2012-12-19 18:27:22 -0300

    Generated the Cardapio Mais application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CardapioMais::Application.config.secret_token = 'f0a099c0c7ecdc6c29244a2730b988e50fb252e53b7c9fc6417ffe1708baaeeda343bc295f816714e8a925f487b9845381bfca67f4671d7d7efe73d4d542ac70'
